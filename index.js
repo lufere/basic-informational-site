237fa4067599796a7a6183892a3f936b3b0f0d73
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-08-25 17:13:15 -0700

    Refactored to express

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var path = require('path');
+var app = express();
 
-http.createServer(function(req,res){
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname + ".html";
-    if(q.pathname == '/') filename = './index.html';
-    fs.readFile(filename, function(err,data){
-        if(err){
-            res.writeHead(404,{'Content-Type':'text/html'});
-            return fs.readFile('./404.html',function(err,data){
-                // res.writeHead(200, {'Content-Type':'text/html'});
-                res.write(data);
-                return res.end();
-            });
-        }
-        res.writeHead(200, {'Content-Type':'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname +'/index.html'));
+});
+
+app.get('/about', function(req,res){
+    res.sendFile(path.join(__dirname +'/about.html'));
+});
+
+app.get('/contact-me', function(req,res){
+    res.sendFile(path.join(__dirname +'/contact-me.html'));
+});
+
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname +'/404.html'));
+});
+
+app.listen(8080, function(){
+    console.log("App listening on port 8080")
+});
